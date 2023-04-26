e2177e98eac5db61026e1b5bf1d7bc208223c1e7
ashmgdesign <80412263+ashmgdesign@users.noreply.github.com>
2023-04-26 15:11:44 +0100

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// Serve the static files from the public folder
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Listen for incoming connections from clients
+io.on('connection', (socket) => {
+    console.log('A user has connected');
+
+    // Listen for incoming chat messages from clients
+    socket.on('chat message', (msg) => {
+        console.log(`Message: ${msg}`);
+        io.emit('chat message', msg); // Broadcast the message to all connected clients
+    });
+
+    // Listen for disconnections from clients
+    socket.on('disconnect', () => {
+        console.log('A user has disconnected');
+    });
+});
+
+// Start the server
+server.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
